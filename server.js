96c4ebfe0ee433933b4643a263597a06aaa57e82
Matthew Bowler <mbowler03@gmail.com>
2020-06-13 08:45:06 -0400

    functioning project but scrape not working

--- server.js
+++ server.js
@@ -4,25 +4,34 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio") //jquery for backend
 var bodyParser = require("body-parser")
 var axios = require("axios");
+var expressHandlebars = require("express-handlebars")
 
-var db = require("./models");
-
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Set up Express Router
+var router = express.Router();
+//require routes file pass router object
+require("./config/routes")(router);
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+app.use(router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine("handlebars", expressHandlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect(db, function(error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("mongoose is loose")
+  }
+});
 
-// Routes
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
